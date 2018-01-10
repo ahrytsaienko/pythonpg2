b381f256404e3944d984b80132e10e6c2315a0e3
a.hrytsaienko <a.hrytsaienko@modnakasta.ua>
2018-01-10 19:53:14 +0200

    connection without with

---
+++ app.py
@@ -0,0 +1,4 @@
+from user import User
+
+my_user = User('johndoe@gmail.com', 'john', 'doe', None)
+my_user.save_to_db()
---
+++ user.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+class User:
+    def __init__(self, email, first_name, last_name, id):
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.id = id
+
+    def __repr__(self):
+        return "<User {}>".format(self.email)
+
+    def save_to_db(self):
+        connection = psycopg2.connect(user='postgres', password='postgres', database='learning', host='localhost')
+        #cursor create queries and work with results
+        with connection.cursor() as cursor:
+            #"with" will automatically close cursor after finish
+            cursor.execute('INSERT INTO users (email, first_name, last_name) VALUES (%s, %s, %s)',
+                            (self.email, self.first_name, self.last_name))
+        connection.commit()
+        connection.close()
+            
+

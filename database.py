10c7955032fc7ea7cdee7c483a670cd79b5fed38
a.hrytsaienko <a.hrytsaienko@modnakasta.ua>
2018-01-12 13:54:46 +0200

    c_poo;

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-import psycopg2
 from psycopg2 import pool
 
 connection_pool = pool.SimpleConnectionPool(1,
@@ -7,3 +6,16 @@ connection_pool = pool.SimpleConnectionPool(1,
                                             password='postgres',
                                             database='learning',
                                             host='localhost')
+
+
+class ConnectionFromPool(object):
+    def __init__(self):
+        self.connection = None
+
+    def __enter__(self):
+        self.connection = connection_pool.getconn()
+        return self.connection
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.commit()
+        connection_pool.putconn(self.connection)
--- user.py
+++ user.py
@@ -1,4 +1,5 @@
-from database import connection_pool
+from database import ConnectionFromPool
+import requests
 
 class User:
     def __init__(self, email, first_name, last_name, id):
@@ -11,23 +12,22 @@ class User:
         return "<User {}>".format(self.email)
 
     def save_to_db(self):
-        connection = connection_pool.getconn()
-        # cursor create queries and work with results
-        with connection.cursor() as cursor:
-            # "with" will automatically close cursor after finish
-            cursor.execute('INSERT INTO users (email, first_name, last_name) VALUES (%s, %s, %s)',
-                            (self.email, self.first_name, self.last_name))
-        connection_pool.putconn(connection)   
+        with ConnectionFromPool() as connection:
+            # cursor create queries and work with results
+            with connection.cursor() as cursor:
+                # "with" will automatically close cursor after finish
+                cursor.execute('INSERT INTO users (email, first_name, last_name) VALUES (%s, %s, %s)',
+                                (self.email, self.first_name, self.last_name))
 
     @classmethod
     def load_from_db_by_email(cls, email):
-        connection = connection_pool.getconn()
-        with connection.cursor() as cursor:
-            cursor.execute(
-                'SELECT * FROM users WHERE email=%s', (email, ))
-            user_data = cursor.fetchone()
-            return cls(id=user_data[0],
-                        email=user_data[1],
-                        first_name=user_data[2],
-                        last_name=user_data[3]
-                        )
+        with ConnectionFromPool() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    'SELECT * FROM users WHERE email=%s', (email, ))
+                user_data = cursor.fetchone()
+                return cls(id=user_data[0],
+                            email=user_data[1],
+                            first_name=user_data[2],
+                            last_name=user_data[3]
+                            )
